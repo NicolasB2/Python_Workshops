2860d9c0392fc35784431968cf5371aa12b91ac7
NicolasBiojo <nikobiober@gmail.com>
2019-05-19 10:35:05 -0500

    Add get and set methods

--- Lab_3 (Car Dealership)/model.py
+++ Lab_3 (Car Dealership)/model.py
@@ -1,4 +1,3 @@
-
 class Dealership:
     def __init__(self,nit,name,address,phone):
         self.nit = nit
@@ -8,7 +7,27 @@ class Dealership:
         self.clients = {}
         self.automobiles = {}
 
-class client:
+    def add_Cliente(self,id,name,bornDate,salary,phone,address,email):
+        c = Client(id,name,bornDate,salary,phone,address,email);
+        self.clients[id]=c;
+
+    def find_Client(self,id):
+        return self.clients.get(id)
+
+    def add_Automobile(self,model, brand, price, reference, color, quantity):
+        a = Automobile(model, brand, price, reference, color, quantity);
+        self.automobiles[model]=a;
+
+    def find_Automobile(self,model):
+        return self.automobiles.get(model)
+
+    def increase_quantity(self,model):
+        self.automobiles.get(model).quantity+=1;
+
+    def decrease_quantity(self,model):
+        self.automobiles.get(model).quantity-=1;
+
+class Client:
     def __init__(self,id,name,bornDate,salary,phone,address,email):
         self.id = id
         self.name = name
@@ -18,7 +37,7 @@ class client:
         self.address = address
         self.email = email
 
-class automobile:
+class Automobile:
     def __init__(self,model, brand, price, reference, color, quantity):
         self.model = model
         self.brand = brand
@@ -26,4 +45,3 @@ class automobile:
         self.reference = reference
         self.color = color
         self.quantity = quantity
-
