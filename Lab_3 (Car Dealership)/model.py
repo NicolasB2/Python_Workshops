7a9151505b92b191e108b90d93b59696cb662bc5
NicolasBiojo <nikobiober@gmail.com>
2019-05-19 18:38:33 -0500

    update model

--- Lab_3 (Car Dealership)/model.py
+++ Lab_3 (Car Dealership)/model.py
@@ -1,11 +1,13 @@
 class Dealership:
     def __init__(self,nit,name,address,phone):
+        self.freeId = 0;
         self.nit = nit
         self.name = name
         self.address = address
         self.phone = phone
         self.clients = {}
         self.automobiles = {}
+        self.plans = {}
 
     def add_Cliente(self,id,name,bornDate,salary,phone,address,email):
         c = Client(id,name,bornDate,salary,phone,address,email);
@@ -27,6 +29,36 @@ class Dealership:
     def decrease_quantity(self,model):
         self.automobiles.get(model).quantity-=1;
 
+    def next_Free_Id(self):
+        self.freeId+=1;
+        return self.freeId;
+
+    def generate_Plan(self,auto,startDate,endDate,numClients,type):
+        x = self.next_Free_Id()
+        f  = Financing_plan(x,auto,startDate,endDate,numClients,type)
+        self.plans[x]=f;
+
+    def find_Plan(self,id):
+        return self.plans.get(id);
+
+
+class Financing_plan:
+    def __init__(self,id,auto,startDate,endDate,numClients,type):
+        self.id=id;
+        self.auto=auto;
+        self.startDate = startDate;
+        self.endDate= endDate;
+        self.numClients=numClients;
+        self.type_plan = generate(type);
+
+
+class type_plan:
+    def __init__(self,name,minSalary,maxSalary,fee):
+        self.name = name;
+        self.minSalary=  minSalary;
+        self.maxSalary=  maxSalary;
+        self.fee = fee;
+
 class Client:
     def __init__(self,id,name,bornDate,salary,phone,address,email):
         self.id = id
@@ -45,3 +77,16 @@ class Automobile:
         self.reference = reference
         self.color = color
         self.quantity = quantity
+
+def generate(self,type):
+
+    if(type == 'basico'):
+            return type_plan('basico',1,2,234000)
+    if(type == 'plata'):
+            return type_plan('plata',3,5,703000)
+    if(type == 'oro'):
+            return type_plan('oro',5,6,1171000)
+    if(type == 'diamante'):
+            return type_plan('diamante',7,8,1640000)
+    if(type == 'premium'):
+            return type_plan('premium',9,10,2109000)
