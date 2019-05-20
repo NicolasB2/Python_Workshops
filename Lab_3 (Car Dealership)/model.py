2df8af22bc97283c158dfc5c42371140cb9c2a89
NicolasBiojo <nikobiober@gmail.com>
2019-05-19 19:27:10 -0500

    Add reports

--- Lab_3 (Car Dealership)/model.py
+++ Lab_3 (Car Dealership)/model.py
@@ -1,6 +1,6 @@
 class Dealership:
     def __init__(self,nit,name,address,phone):
-        self.freeId = 0;
+        self.freeId = 0
         self.nit = nit
         self.name = name
         self.address = address
@@ -10,54 +10,85 @@ class Dealership:
         self.plans = {}
 
     def add_Cliente(self,id,name,bornDate,salary,phone,address,email):
-        c = Client(id,name,bornDate,salary,phone,address,email);
-        self.clients[id]=c;
+        c = Client(id,name,bornDate,salary,phone,address,email)
+        self.clients[id]=c
 
     def find_Client(self,id):
         return self.clients.get(id)
 
     def add_Automobile(self,model, brand, price, reference, color, quantity):
-        a = Automobile(model, brand, price, reference, color, quantity);
-        self.automobiles[model]=a;
+        a = Automobile(model, brand, price, reference, color, quantity)
+        self.automobiles[model]=a
 
     def find_Automobile(self,model):
         return self.automobiles.get(model)
 
     def increase_quantity(self,model):
-        self.automobiles.get(model).quantity+=1;
+        self.automobiles.get(model).quantity+=1
 
     def decrease_quantity(self,model):
-        self.automobiles.get(model).quantity-=1;
+        self.automobiles.get(model).quantity-=1
 
     def next_Free_Id(self):
-        self.freeId+=1;
-        return self.freeId;
+        self.freeId+=1
+        return self.freeId
 
     def generate_Plan(self,auto,startDate,endDate,numClients,type):
         x = self.next_Free_Id()
         f  = Financing_plan(x,auto,startDate,endDate,numClients,type)
-        self.plans[x]=f;
+        self.plans[x]=f
 
     def find_Plan(self,id):
-        return self.plans.get(id);
+        return self.plans.get(id)
+
+    def report_Plan(self,id):
+        p = self.find_Plan(id)
 
 
 class Financing_plan:
     def __init__(self,id,auto,startDate,endDate,numClients,type):
-        self.id=id;
-        self.auto=auto;
-        self.startDate = startDate;
-        self.endDate= endDate;
-        self.numClients=numClients;
-        self.type_plan = generate(type);
+        self.id=id
+        self.auto=auto
+        self.startDate = startDate
+        self.endDate= endDate
+        self.numClients=numClients
+        self.type_plan = generate(type)
+        self.clients = {}
 
+    def add_Cliente(self,id,name,bornDate,salary,phone,address,email):
+        c = Client(id,name,bornDate,salary,phone,address,email)
+        self.clients[id] = c
+
+    def find_Client(self,id):
+        return self.clients.get(id)
+
+    def report(self):
+        x  = "\tId: "+self.id+ "\n"
+        x += "\tStart Date: " + self.startDate + "\n"
+        x += "\tEnd Date: " + self.endDate + "\n\n"
+        x += "\t" + self.type_plan.report() + "\n"
+
+        x += "\t"+'Clients:'
+        for data in self.clients:
+            x += data.report()+"\n"
+
+        x += "\t"+'Automobile'
+        for data in self.auto:
+            x += data.report()+"\n"
 
 class type_plan:
     def __init__(self,name,minSalary,maxSalary,fee):
-        self.name = name;
-        self.minSalary=  minSalary;
-        self.maxSalary=  maxSalary;
-        self.fee = fee;
+        self.name = name
+        self.minSalary= minSalary
+        self.maxSalary= maxSalary
+        self.fee = fee
+
+    def report(self):
+        x  = "\tName: "+self.name+ "\n"
+        x += "\tMin Salary: " + self.minSalary+ "\n"
+        x += "\tMax Salary: " + self.maxSalary+ "\n"
+        x += "\tFee: " + self.fee
+        return x;
 
 class Client:
     def __init__(self,id,name,bornDate,salary,phone,address,email):
@@ -69,6 +100,16 @@ class Client:
         self.address = address
         self.email = email
 
+    def report(self):
+        x = "\tId: " + self.id + "\n"
+        x+= "\tName: " + self.name + "\n"
+        x+= "\tBorn Date: " + self.bornDate + "\n"
+        x+= "\tSalary: "+self.salary + "\n"
+        x+= "\tPhone: "+self.phone + "\n"
+        x+= "\tAddress: "+self.address + "\n"
+        x+= "\tEmail: "+self.email
+        return x
+
 class Automobile:
     def __init__(self,model, brand, price, reference, color, quantity):
         self.model = model
@@ -77,6 +118,15 @@ class Automobile:
         self.reference = reference
         self.color = color
         self.quantity = quantity
+
+    def report_Automobile(self):
+        x =  "\tName: " + self.model + "\n"
+        x += "\tBrand: " + self.brand + "\n"
+        x += "\tPrice: " + self.price + "\n"
+        x += "\tReference: " + self.reference + "\n"
+        x += "\tColor: " + self.color + "\n"
+        x += "\tQuantity: " + self.quantity
+        return x
 
 def generate(self,type):
 
---
+++ Lab_3 (Car Dealership)/view.py
@@ -0,0 +1,5 @@
+from model import *;
+class gui:
+    def __init__(self):
+        self.dealership = Dealership('A0','carNow','#4567','52237876');
+
